24364f3afd7915d13abf4af9a814693c2dd2ad44
jackychen00758 <jackychen00758@intumit.com>
2020-12-22 17:04:45 +0800

    調整主頁面的UI

--- SpendTracker/Form1.Designer.cs
+++ SpendTracker/Form1.Designer.cs
@@ -37,26 +37,29 @@ namespace SpendTracker
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(44, 36);
+            this.comboBox1.Location = new System.Drawing.Point(28, 23);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(182, 31);
+            this.comboBox1.Size = new System.Drawing.Size(117, 23);
             this.comboBox1.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(44, 96);
+            this.panel1.Location = new System.Drawing.Point(28, 63);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 150);
+            this.panel1.Size = new System.Drawing.Size(191, 98);
             this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(509, 293);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.comboBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "SpendTracker";
             this.Load += new System.EventHandler(this.Form1_Load);
--- SpendTracker/Form1.cs
+++ SpendTracker/Form1.cs
@@ -20,7 +20,7 @@ namespace SpendTracker
 
             // 計算主視窗對應該解析度的寬與高
             int RelativeWidth = (int)(Convert.ToInt32(DesktopWidthStr) * 0.8);
-            int RelativeHeight = (int)(Convert.ToInt32(DesktopHeightStr) * 0.8);
+            int RelativeHeight = (int)(Convert.ToInt32(DesktopHeightStr) * 0.9);
 
             // 調整主視窗的大小
             Size = new Size(RelativeWidth, RelativeHeight);
@@ -47,12 +47,15 @@ namespace SpendTracker
             int x = (int)(Size.Width * 0.495) - (comboBox1.Width) / 2;
             int y = (int)(Size.Height * 0.03);
             comboBox1.Location = (Point)new Size(x, y);
+
+            // 預設內容
+            comboBox1.Text = DateTime.Now.ToString("yyyy年MM月");
         }
 
         public void ArrangePanel1()
         {
-            // 設置相對寬度與高度
-            panel1.Size = new Size((int)(Size.Width * 0.9), (int)(Size.Height * 0.7));
+            // 設置相對寬度 & 將高度固定比較不容易跑版
+            panel1.Size = new Size((int)(Size.Width * 0.9), 504);
 
             // 設置相對位置
             int x = (int)(Size.Width * 0.495) - (panel1.Width) / 2;
@@ -87,7 +90,7 @@ namespace SpendTracker
             // 創建並調整標題列
             AddTitleBar();
 
-            // 創建並插入支出資料(預設為當年當月)
+            // 創建並寫入每日的花費資料(預設為當年當月)
             AddDailySpend(DateTime.Now.Year, DateTime.Now.Month);
 
             // 令表格和容器高度相同
@@ -111,24 +114,31 @@ namespace SpendTracker
 
                 // Height 可以影響字體在格子中的垂直位置，設太大也不會影響顯示
                 // Width 可以影響字體在格子中的水平位置
-                int horizontalShift = (int)(Size.Width / 4.8);
-                int verticalShift = 100;
-                int fontSize = 10;
 
                 List<Button> Buttons = new List<Button>();
 
+                // 設置按鈕並將按鈕添加到表格
                 for(int i = 0; i < 7; i++)
                 {
                     Buttons.Add(new Button
                     {
-                        Width = horizontalShift * 2,
-                        Height = verticalShift,
-                        Font = new Font("Microsoft JhengHei", fontSize, FontStyle.Regular),
+                        Height = 40,
+                        Font = new Font("Microsoft JhengHei", 10, FontStyle.Regular),
                         TextAlign = ContentAlignment.MiddleCenter
                     });
                     table.Controls.Add(Buttons.Last(), i, j);
                 }
 
+                // 令各按鈕的寬度等於各欄位的寬度
+                Buttons[0].Width = (int)(table.Width * 0.1228);
+                Buttons[1].Width = (int)(table.Width * 0.1228);
+                Buttons[2].Width = (int)(table.Width * 0.1228);
+                Buttons[3].Width = (int)(table.Width * 0.1228);
+                Buttons[4].Width = (int)(table.Width * 0.1228);
+                Buttons[5].Width = (int)(table.Width * 0.3056);
+                Buttons[6].Width = (int)(table.Width * 0.0856);
+
+                // 設置各按鈕的文字內容
                 Buttons[0].Text = "日期";
                 Buttons[1].Text = "伙食費";
                 Buttons[2].Text = "交通費";
@@ -147,46 +157,51 @@ namespace SpendTracker
         {
             try
             {
-                // 預設為第一頁，寫入18筆資料
-                for (int day = 1; day <= 18; day++)
+                // 預設為第一頁，寫入17筆資料(前16行為1~16日的花費，第17行為整個月的各項加總)
+                for (int day = 1; day <= 17; day++)
                 {
                     // 動態新增一行
                     table.RowCount++;
 
                     // 設定這一行的高度
-                    table.RowStyles.Add(new RowStyle(SizeType.Absolute, 28));
+                    table.RowStyles.Add(new RowStyle(SizeType.Absolute, 24));
+                    int cellHeight = 24;
 
                     // 令每個欄位都顯示邊框
                     table.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetPartial;
 
                     int j = table.RowCount - 1;
 
-                    // Height 可以影響字體在格子中的垂直位置，設太大也不會影響顯示
-                    // Width 可以影響字體在格子中的水平位置
-                    int horizontalShift = (int)(Size.Width / 4.8);
-                    int verticalShift = 100;
-                    int fontSize = 10;
 
+                    // 建立這筆資料的各個cell並添加到表格
                     List<Label> labels = new List<Label>();
                     for (int i = 0; i < 7; i++)
                     {
                         labels.Add(new Label
                         {
-                            Width = horizontalShift,
-                            Height = verticalShift,
-                            Font = new Font("Microsoft JhengHei", fontSize, FontStyle.Regular),
+                            Height = cellHeight,
+                            Font = new Font("Microsoft JhengHei", 10, FontStyle.Regular),
                             TextAlign = ContentAlignment.MiddleCenter
                         });
                         table.Controls.Add(labels.Last(), i, j);
                     }
 
-                    // 載入該日期的資料
+                    // 令各cell的寬度等於各欄位的寬度
+                    labels[0].Width = (int)(table.Width * 0.1228);
+                    labels[1].Width = (int)(table.Width * 0.1228);
+                    labels[2].Width = (int)(table.Width * 0.1228);
+                    labels[3].Width = (int)(table.Width * 0.1228);
+                    labels[4].Width = (int)(table.Width * 0.1228);
+                    labels[5].Width = (int)(table.Width * 0.3056);
+                    labels[6].Width = (int)(table.Width * 0.0856);
+
+                    // 將對應日期的資料寫入到各個cell
                     labels[0].Text = "2020-12-21";
                     labels[1].Text = day.ToString();
                     labels[2].Text = "123";
                     labels[3].Text = "123";
                     labels[4].Text = "123";
-                    labels[5].Text = "測試測試測試測試測試測試測試測試測試測試";
+                    labels[5].Text = "測試測試測試測試測試測試測試測試測試";
                     labels[6].Text = "編輯";
                 }
             }
